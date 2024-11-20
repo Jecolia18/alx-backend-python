768ebbe37a1eea7c4f92dd26185c7b929b3e8c00
Jecolia <jecoliatieleo@gmail.com>
2024-11-20 04:45:54 -0800

    what is up with floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """this is a function to return floor"""
 
-
 import math
+"""math module imported"""
+
+
 def floor(n: float) -> int:
     """return the floor of n"""
     return math.floor(n)
