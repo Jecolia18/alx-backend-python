74cd8e52ca4b836e6eb60011fffcdd1a2128f12e
Jecolia <jecoliatieleo@gmail.com>
2024-11-21 03:44:07 -0800

    I did not know the typing module existed

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,7 +2,8 @@
 """type-annotated function"""
 from typing import List
 
-def sum_list(input_list: List[float] ) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """funcions that returns the sum of a list"""
     su = 0
     for i in input_list:
