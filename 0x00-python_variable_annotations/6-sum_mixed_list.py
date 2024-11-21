43f48ea5a06bebe1f11e401b89e7e7f60d8e2bf0
Jecolia <jecoliatieleo@gmail.com>
2024-11-21 04:02:34 -0800

    I did not know it was possible to combine multiple types in a list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,6 +2,7 @@
 """a function that does the sum"""
 from typing import List, Union
 
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """adds integers and floats"""
     return float(sum(mxd_lst))
