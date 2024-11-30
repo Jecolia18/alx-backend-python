7a3a5833122b26d25b26056e06a175a57e76197f
Jecolia <jecoliatieleo@gmail.com>
2024-11-30 14:08:12 -0800

    help us

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""delay async"""
+import asyncio
+import random
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """returns in ascending order floats number"""
+
+    waits: List[float] = []
+    tasks: List = []
+
+    for _ in range(n):
+        tasks.append(wait_random(max_delay))
+
+    for task in asyncio.as_completed((tasks)):
+        wait = await task
+        waits.append(wait)
+
+    return waits
