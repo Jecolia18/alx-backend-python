f04484c5c56ab8b89d2e5ecc4ede5d3d63b3d199
Jecolia <jecoliatieleo@gmail.com>
2024-12-02 12:44:23 -0800

    how

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,9 @@
+#!/usr/bin env python3
+"""genereate a list """
+from typing import List
+async_generator = __import__('async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """return the generated list"""
+    return [_async for _ in async_generator()]
