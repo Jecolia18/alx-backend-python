91677b70769ae6cd284aff2253fdf99a6272fbfc
Jecolia <jecoliatieleo@gmail.com>
2024-12-03 01:45:23 -0800

    graduation sake

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -10,4 +10,4 @@ async def measure_runtime() -> float:
     start_time = time.perf_counter()
     await asyncio.gather(async_comprehension(), async_comprehension(),
             async_comprehension(), async_comprehension())
-    return time.perf_counter - start_time
+    return time.perf_counter() - start_time
